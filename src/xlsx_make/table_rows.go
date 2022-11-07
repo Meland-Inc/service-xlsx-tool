390e8f9a19c556e97a71fc3dbca290423d3f2566
alextjj <254334386@qq.com>
2022-11-07 20:14:30 +0800

    fix up add task option api

--- src/xlsx_make/table_rows.go
+++ src/xlsx_make/table_rows.go
@@ -1,6 +1,8 @@
 package xlsx_make
 
 import (
+	xlsxTable "game-message-core/xlsxTableData"
+
 	"github.com/Meland-Inc/service-xlsx-tool/src/common/serviceLog"
 	"github.com/Meland-Inc/service-xlsx-tool/src/xlsx_make/data"
 )
@@ -15,5 +17,26 @@ func AddTaskXlsxData(xlsxRow data.TaskXlsxRow) {
 		}
 	}
 
+	for i := 0; i < len(taskTableRows)-1; i++ {
+		for j := i + 1; j < len(taskTableRows); j++ {
+			if taskTableRows[i].Id > taskTableRows[j].Id {
+				taskTableRows[i], taskTableRows[j] = taskTableRows[j], taskTableRows[i]
+			}
+		}
+	}
+
 	taskTableRows = append(taskTableRows, xlsxRow)
+}
+
+func AddTaskOption(
+	id int32,
+	designateOptions []xlsxTable.TaskXlsxOption,
+	chanceOptions []xlsxTable.TaskXlsxOption,
+) {
+	row := data.TaskXlsxRow{
+		Id:               id,
+		DesignateOptions: designateOptions,
+		ChanceOptions:    chanceOptions,
+	}
+	AddTaskXlsxData(row)
 }
