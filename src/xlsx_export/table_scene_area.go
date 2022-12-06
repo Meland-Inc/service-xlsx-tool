5f5c72eb2b0107a24a0b4b913c05f653b48a1219
alextjj <254334386@qq.com>
2022-12-06 16:29:48 +0800

    load scene area table

---
+++ src/xlsx_export/table_scene_area.go
@@ -0,0 +1,39 @@
+package xlsx_export
+
+import (
+	"time"
+
+	xlsxTable "game-message-core/xlsxTableData"
+
+	"github.com/Meland-Inc/service-xlsx-tool/src/common/excel"
+	"gorm.io/gorm"
+)
+
+// SceneArea.xlsx
+
+var sceneAreaRows = make(map[int32]xlsxTable.SceneAreaRow)
+
+func ParseSceneArea(rows []map[string]interface{}) (err error) {
+	for _, row := range rows {
+		if row["id"] == "" || row["id"] == "0" {
+			continue
+		}
+
+		setting := xlsxTable.SceneAreaRow{
+			Id:   excel.IntToInt32(row["id"]),
+			Name: excel.StringToString(row["sceneName"]),
+		}
+
+		sceneAreaRows[setting.Id] = setting
+	}
+	return err
+}
+
+func SceneAreaSaveToDB(db *gorm.DB, curSecUtc time.Time) {
+	list := []xlsxTable.SceneAreaRow{}
+	for _, row := range sceneAreaRows {
+		row.CreatedAt = curSecUtc
+		list = append(list, row)
+	}
+	WriterToDB(db, curSecUtc, &xlsxTable.SceneAreaRow{}, len(list), list)
+}
