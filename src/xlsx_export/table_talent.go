aeaec859c5f08ade66d101742d61a99f58c086e2
alextjj <254334386@qq.com>
2023-01-09 17:43:30 +0800

    export table 'talent' to DB

--- src/xlsx_export/table_talent.go
+++ src/xlsx_export/table_talent.go
@@ -25,7 +25,7 @@ func ParseTalentTree(rows []map[string]interface{}) (err error) {
 			IsTrunk:    excel.BoolToBool(row["isTrunk"]),
 			Layer:      excel.IntToInt32(row["layer"]),
 			LvLimit:    excel.IntToInt32(row["lvLimit"]),
-			Mode:       excel.IntToInt32(row["mode"]),
+			Gains:      excel.IntToInt32(row["gains"]),
 		}
 		upgradeData := xlsxTable.TalentUpgradeData{
 			TalentTreeLv:  excel.IntToInt32(row["upgradeRequireTreeLv"]),
